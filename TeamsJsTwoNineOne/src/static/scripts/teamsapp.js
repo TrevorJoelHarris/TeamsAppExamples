343825145dc00c49a53f716ebbd1ea92e0879a7e
Trevor Harris <trharris@microsoft.com>
2023-08-31 10:38:51 -0700

    Simple teams app using teamsjs 2.9.1

---
+++ TeamsJsTwoNineOne/src/static/scripts/teamsapp.js
@@ -0,0 +1,18 @@
+(async function () {
+  "use strict";
+
+  // Call the initialize API first
+  await microsoftTeams.app.initialize();
+
+  microsoftTeams.app.getContext().then(function (context) {
+    if (context?.app?.host?.name) {
+      updateHubName(context.app.host.name);
+    }
+  });
+
+  function updateHubName(hubName) {
+    if (hubName) {
+      document.getElementById("hubName").innerHTML = hubName;
+    }
+  }
+})();
